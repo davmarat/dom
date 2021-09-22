eada5dc20299b48eb84f074f08c96a974fb06f0b
davmarat <davletgareev.mn@gmail.com>
2021-09-22 17:07:55 +0300

    commit

---
+++ src/js/__test__/app.test.js
@@ -0,0 +1,14 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import GamePlay from '../GamePlay';
+
+test('check gameplay', () => {
+  function checkGamePlay() {
+    const gameplay = new GamePlay();
+    gameplay.bindToDom(null);
+  }
+
+  expect(checkGamePlay).toThrowError(new Error('container is not HTMLElement'));
+});
